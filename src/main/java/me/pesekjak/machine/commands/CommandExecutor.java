923bd353ee3bc0ed13d67c687c370d78a952873a
_tud <mmbakkar06@gmail.com>
2022-11-20 20:23:29 +0300

    cleanup CommandExecutor#formatCommandInput

--- src/main/java/me/pesekjak/machine/commands/CommandExecutor.java
+++ src/main/java/me/pesekjak/machine/commands/CommandExecutor.java
@@ -6,16 +6,9 @@ public interface CommandExecutor {
 
     static String formatCommandInput(String input) {
         if(input.length() == 0) return "";
+        input = input.trim();
         while(input.contains("  "))
             input = input.replace("  ", " ");
-        while(input.charAt(input.length() - 1) == ' ') {
-            input = input.substring(0, input.length() - 1);
-            if(input.length() == 0) return "";
-        }
-        while(input.charAt(0) == ' ') {
-            input = input.substring(1);
-            if(input.length() == 0) return "";
-        }
         return input;
     }
 
