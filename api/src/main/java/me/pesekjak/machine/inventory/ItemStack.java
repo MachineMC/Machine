6dedc65b31938543a41b6e3cc06f9c9c7fe0f6e9
Pesek <jakub.zahomolkou@gmail.com>
2022-12-06 18:00:00 +0100

    moved item stack (default item implementation) to the api

--- api/src/main/java/me/pesekjak/machine/inventory/ItemStack.java
+++ api/src/main/java/me/pesekjak/machine/inventory/ItemStack.java
@@ -1,15 +1,12 @@
 package me.pesekjak.machine.inventory;
 
-import com.google.common.base.Objects;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
-import me.pesekjak.machine.utils.FriendlyByteBuf;
-import me.pesekjak.machine.utils.ServerBuffer;
 import me.pesekjak.machine.world.Material;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.jglrxavpok.hephaistos.nbt.NBT;
 import org.jglrxavpok.hephaistos.nbt.NBTCompound;
 import org.jglrxavpok.hephaistos.nbt.NBTException;
 import org.jglrxavpok.hephaistos.parser.SNBTParser;
@@ -17,8 +14,9 @@ import org.jglrxavpok.hephaistos.parser.SNBTParser;
 import java.io.StringReader;
 
 /**
- * Represents an item in inventory.
+ * Default item implementation.
  */
+@EqualsAndHashCode
 public class ItemStack implements Item {
 
     private final static Material[] REGISTRY;
@@ -39,7 +37,7 @@ public class ItemStack implements Item {
     }
 
     /**
-     * Returns a material from id (mapped by vanilla server reports)
+     * Returns a material from id (mapped by vanilla server reports).
      * @param id id of the material
      * @return material with the id
      */
@@ -51,7 +49,7 @@ public class ItemStack implements Item {
     }
 
     /**
-     * Returns id of the material
+     * Returns id of the material.
      * @param material material to get id from
      * @return id of the material
      */
@@ -66,27 +64,27 @@ public class ItemStack implements Item {
         this.material = material;
     }
 
-    public ItemStack(Material material, byte amount) {
+    public ItemStack(@NotNull Material material, byte amount) {
         this(material);
         this.amount = amount;
     }
 
     @Override
-    public @NotNull Item withMaterial(@NotNull Material material) {
+    public @NotNull ItemStack withMaterial(@NotNull Material material) {
         final ItemStack itemStack = clone();
         itemStack.setMaterial(material);
         return itemStack;
     }
 
     @Override
-    public @NotNull Item withAmount(byte amount) {
+    public @NotNull ItemStack withAmount(byte amount) {
         final ItemStack itemStack = clone();
         itemStack.setAmount(amount);
         return itemStack;
     }
 
     @Override
-    public @NotNull Item withNbtCompound(NBTCompound compound) {
+    public @NotNull ItemStack withNbtCompound(NBTCompound compound) {
         final ItemStack itemStack = clone();
         itemStack.setNbtCompound(compound);
         return itemStack;
@@ -103,87 +101,37 @@ public class ItemStack implements Item {
     }
 
     @Override
-    public @NotNull Item withType(Material type) {
+    public @NotNull ItemStack withType(Material type) {
         final ItemStack itemStack = clone();
         itemStack.setMaterial(type);
         return itemStack;
     }
 
-    /**
-     * Changes the NBT of the item to NBT of given string.
-     * @param nbtCompound new nbt of the ItemStack
-     */
     @Override
     public void writeNBT(@NotNull String nbtCompound) throws NBTException {
         this.nbtCompound = (NBTCompound) new SNBTParser(new StringReader(nbtCompound)).parse();
     }
 
-    /**
-     * Resets the NBT of the ItemStack
-     */
     @Override
     public void removeNBT() {
         nbtCompound = new NBTCompound();
     }
 
-    /**
-     * Adds given amount to the ItemStack.
-     * @param amount amount to add
-     */
     @Override
     public void add(byte amount) {
         this.amount += amount;
     }
 
-    /**
-     * Removes given amount from the ItemStack
-     * @param amount amount to remove
-     */
     @Override
     public void subtract(byte amount) {
         this.amount -= amount;
     }
 
-    /**
-     * @return the copy of the ItemStack with amount of 1
-     */
     @Override
     public @NotNull ItemStack single() {
         ItemStack single = clone();
         single.amount = 1;
         return single;
-    }
-
-    @Override
-    public byte @NotNull [] serialize() {
-        return serialize(this);
-    }
-
-    public static @NotNull ItemStack deserialize(byte[] bytes) {
-        FriendlyByteBuf buf = new FriendlyByteBuf(bytes);
-        if(!buf.readBoolean())
-            return new ItemStack(Material.AIR);
-        ItemStack itemStack = new ItemStack(getMaterial(buf.readVarInt()), buf.readByte());
-        NBT nbt = buf.readNBT();
-        if(nbt instanceof NBTCompound)
-            itemStack.nbtCompound = (NBTCompound) nbt;
-        return itemStack;
-    }
-
-    public static byte[] serialize(@NotNull ItemStack itemStack) {
-        FriendlyByteBuf buf = new FriendlyByteBuf();
-        if(itemStack.material == Material.AIR) {
-            buf.writeBoolean(false);
-            return buf.bytes();
-        }
-        buf.writeBoolean(true);
-        buf.writeVarInt(itemStack.material.getId());
-        buf.writeByte(itemStack.amount);
-        if(itemStack.nbtCompound.getSize() != 0)
-            buf.writeNBT("", itemStack.nbtCompound);
-        else
-            buf.writeBoolean(false);
-        return buf.bytes();
     }
 
     @Override
@@ -200,24 +148,6 @@ public class ItemStack implements Item {
     @Override
     public String toString() {
         return amount + "x" + material + nbtCompound.toSNBT();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof ItemStack itemStack)) return false;
-        return amount == itemStack.amount && material == itemStack.material &&
-                Objects.equal(nbtCompound, itemStack.nbtCompound);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(material, amount, nbtCompound);
-    }
-
-    @Override
-    public void write(@NotNull ServerBuffer buf) {
-        buf.writeSlot(this);
     }
 
 }
